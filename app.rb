27cd5d32cd661124b7d096121b875e2c4ada8939
Hesham Farag <heshamf@gmail.com>
2017-07-18 16:49:17 +0100

    Can view player 2 score

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+	enable :sessions
+
 	# get '/' do
 	#   "Testing infrastructure working!"
 	# end
@@ -11,12 +13,19 @@ class Battle < Sinatra::Base
 	end
 
 	post '/names' do
-		p params
-		@player_1_name = params[:player_1_name]
-		@player_2_name = params[:player_2_name]
-		erb :play
+  		session[:player_1_name] = params[:player_1_name]
+  		session[:player_2_name] = params[:player_2_name]
+  		redirect '/play'
 	end
 
-run! if app_file == $0
+
+	get '/play' do
+  		@player_1_name = session[:player_1_name]
+  		@player_2_name = session[:player_2_name]
+  	erb :play
+	end		
+
+
+	run! if app_file == $0
 
 end
